532ad0ec706e2a9bae05e830c31917c87b37cf0d
Wee Jian Hui Kevin <hello@codebykevin.dev>
2024-03-06 16:06:03 +0800

    feat: add components for viewer and customiser

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import { useState } from 'react'
+import Customiser from './customiser/Customiser';
+import Viewer from './viewer/Viewer';
+import defaultData from "./defaultData.json";
 
 function App() {
 
+  const [cvData, setCvData] = useState(defaultData);
   return (
     <>
-      <h1>hello, world</h1>
+      <div className="flex flex-col md:flex-row">
+        <Customiser cvData={cvData} setCvData={setCvData} />
+        <Viewer cvData={cvData} />
+      </div>
     </>
   )
 }
