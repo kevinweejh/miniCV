645036b5ea8261bdd60bbbe5164dbefa1516a173
Wee Jian Hui Kevin <hello@codebykevin.dev>
2024-03-08 21:57:08 +0800

    feat: hook data state up to form submission
    Modify form, and create updater function to
    update the `generalInfo` section of the data
    state.

--- src/customiser/components/CustomiserGeneral.jsx
+++ src/customiser/components/CustomiserGeneral.jsx
@@ -5,21 +5,36 @@ import { useState } from 'react'
 
 const CustomiserGeneral = ({ info, setInfo }) => {
     const [value, setValue] = useState();
+
+    const handleGeneralInfoSave = (e) => {
+        e.preventDefault();
+        
+        const updatedGeneralInfo = { 
+            ...info, 
+            firstName: e.target.firstNameInput.value,
+            lastName: e.target.lastNameInput.value,
+            email: e.target.emailInput.value,
+            mobile: value
+        }
+        setInfo(updatedGeneralInfo);
+    };
+
     return(
         <>
             <details>
                 <summary className="flex flex-col p-2 text-2xl font-semibold text-center md:text-left hover:cursor-pointer hover:bg-gray-100">General</summary>
                 <div className="p-4 bg-gray-200">
-                    <form className="flex flex-col">
+                    <form className="flex flex-col" onSubmit={handleGeneralInfoSave}>
                         <label htmlFor="firstNameInput">First Name: </label>
-                        <input type="text" id="firstNameInput" placeholder="John"></input>
+                        <input type="text" id="firstNameInput" name="firstNameInput" placeholder="John"></input>
                         <label htmlFor="lastNameInput">Last Name: </label>
-                        <input type="text" id="lastNameInput" placeholder="Lee"></input>
+                        <input type="text" id="lastNameInput" name="lastNameInput" placeholder="Lee"></input>
                         <label htmlFor="emailInput">E-Mail: </label>
-                        <input type="email" id="emailInput" placeholder="john.lee@gmail.com"></input>
+                        <input type="email" id="emailInput" name="emailInput" placeholder="john.lee@gmail.com"></input>
                         <label htmlFor="mobileInput">Mobile No.: </label>
                         <PhoneInput 
                             id="mobileInput"
+                            name="mobileInput"
                             placeholder="Enter phone number"
                             value={value}
                             onChange={setValue} />
