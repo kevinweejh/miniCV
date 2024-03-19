64559cfa5d78df6d449a906443bdb3ff4e252537
Wee Jian Hui Kevin <hello@codebykevin.dev>
2024-03-19 16:52:27 +0800

    feat: add components for displaying achievements
    Add a ViewerItemAchievementsList parent component
    which renders an unordered list, and enclosed
    within it is a .map method to render child
    component ViewerItemAchievement for each
    achievement.

---
+++ src/viewer/components/ViewerItemAchievement.jsx
@@ -0,0 +1,13 @@
+import PropTypes from 'prop-types';
+
+const ViewerItemAchievement = ({ text }) => {
+    return(
+        <li className="list-disc list-inside">{text}</li>
+    )
+}
+
+ViewerItemAchievement.propTypes = {
+    text: PropTypes.string.isRequired,
+};
+
+export default ViewerItemAchievement;
---
+++ src/viewer/components/ViewerItemAchievementsList.jsx
@@ -0,0 +1,23 @@
+import PropTypes from 'prop-types';
+import ViewerItemAchievement from './ViewerItemAchievement';
+
+const ViewerItemAchievementsList = ({ achievementsList }) => {
+    return(
+        <ul>
+            {achievementsList.map((achievement) => 
+                <ViewerItemAchievement key={achievement.id} text={achievement.text} />
+            )}
+        </ul>
+    )
+}
+
+ViewerItemAchievementsList.propTypes = {
+    achievementsList: PropTypes.arrayOf(
+        PropTypes.shape({
+            id: PropTypes.number,
+            text: PropTypes.string,
+        })
+    ).isRequired
+};
+
+export default ViewerItemAchievementsList;
