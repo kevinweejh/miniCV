da9b2b9871fb48729d686a98b6fd55a507c787c6
Wee Jian Hui Kevin <hello@codebykevin.dev>
2024-03-19 16:59:07 +0800

    feat: add components for handling achievements
    Add CustomiserItemAchievementsList parent
    component which renders the child components for
    each achievement.
    
    Add mechanism to add multiple achievements for
    each Customiser 'item'.

---
+++ src/customiser/components/CustomiserItemAchievement.jsx
@@ -0,0 +1,13 @@
+import PropTypes from 'prop-types';
+
+const CustomiserItemAchievement = ({ text }) => {
+    return(
+        <li className="list-disc list-inside">{text}</li>
+    )
+}
+
+CustomiserItemAchievement.propTypes = {
+    text: PropTypes.string.isRequired,
+};
+
+export default CustomiserItemAchievement;
---
+++ src/customiser/components/CustomiserItemAchievementsList.jsx
@@ -0,0 +1,38 @@
+import PropTypes from 'prop-types';
+import { useState } from 'react';
+import CustomiserItemAchievement from './CustomiserItemAchievement';
+
+const CustomiserItemAchievementsList = ({ achievementsList, handleAchievementsListAdd }) => {
+    const [newAchievement, setNewAchievement] = useState('');
+
+    const handleAddClick = () => {
+        if (newAchievement.trim()) { // Check if the input is not just empty spaces
+            handleAchievementsListAdd(newAchievement);
+            setNewAchievement(''); // Reset the input field
+        }
+    }
+
+    return(
+        <>
+            <ul>
+                {achievementsList.map((achievement) => 
+                    <CustomiserItemAchievement key={achievement.id} text={achievement.text} />
+                )}
+            </ul>
+            <input id="newAchievement" type="text" value={newAchievement} onChange={e => setNewAchievement(e.target.value)} placeholder="Enter achievement"></input>
+            <button onClick={handleAddClick} type="button" className="border rounded-md w-fit mt-4 ml-auto px-2 border-gray-400 hover:bg-gray-400">Add achievement</button>
+        </>
+    )
+}
+
+CustomiserItemAchievementsList.propTypes = {
+    achievementsList: PropTypes.arrayOf(
+        PropTypes.shape({
+            id: PropTypes.number,
+            text: PropTypes.string,
+        })
+    ).isRequired,
+    handleAchievementsListAdd: PropTypes.func.isRequired,
+};
+
+export default CustomiserItemAchievementsList;
