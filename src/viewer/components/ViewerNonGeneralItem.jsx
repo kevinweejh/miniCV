bd2841d8c86f945cea5a010fde8610cbdf585473
Wee Jian Hui Kevin <hello@codebykevin.dev>
2024-03-27 21:47:32 +0800

    chore: improve wording on resume reader
    Align wording with professional resume template

--- src/viewer/components/ViewerNonGeneralItem.jsx
+++ src/viewer/components/ViewerNonGeneralItem.jsx
@@ -1,18 +1,37 @@
 import PropTypes from 'prop-types';
 import ViewerItemAchievementsList from './ViewerItemAchievementsList';
+import dayjs from 'dayjs';
 
 const ViewerNonGeneralItem = ({ nonGeneralSectionItem }) => {
+    const formType = nonGeneralSectionItem.formType;
+    const educationDateToObject = dayjs(nonGeneralSectionItem.yearTo);
+    const graduated = educationDateToObject.isBefore(dayjs());
     return (
         <>
-            <h1 className="text-lg font-semibold">{nonGeneralSectionItem.orgName}</h1>
-            <div className="flex flex-row justify-between mb-2">
-                <p className="text-left">{nonGeneralSectionItem.position}</p>
-                <p className="text-right">
-                    {nonGeneralSectionItem.yearFrom} –{' '}
-                    {nonGeneralSectionItem.currentStatus ? 'Present' : nonGeneralSectionItem.yearTo}
-                </p>
-            </div>
-            <ViewerItemAchievementsList achievementsList={nonGeneralSectionItem.achievementsList} />
+            {formType === 'education' ? (
+                <>
+                    <div className="flex flex-row justify-between">
+                        <p className="text-lg font-semibold text-left">{nonGeneralSectionItem.orgName}</p>
+                        <p className="text-right">
+                            {graduated ? nonGeneralSectionItem.yearTo : `Expected ${nonGeneralSectionItem.yearTo}`}
+                        </p>
+                    </div>
+
+                    <p className="text-base font-normal"> &#8226; {nonGeneralSectionItem.position}</p>
+                </>
+            ) : (
+                <>
+                    <h1 className="text-lg font-semibold">{nonGeneralSectionItem.orgName}</h1>
+                    <div className="flex flex-row justify-between mb-2">
+                        <p className="text-left">{nonGeneralSectionItem.position}</p>
+                        <p className="text-right">
+                            {nonGeneralSectionItem.yearFrom} &#8211;{' '}
+                            {nonGeneralSectionItem.currentStatus ? 'Present' : nonGeneralSectionItem.yearTo}
+                        </p>
+                    </div>
+                    <ViewerItemAchievementsList achievementsList={nonGeneralSectionItem.achievementsList} />
+                </>
+            )}
         </>
     );
 };
@@ -31,6 +50,7 @@ ViewerNonGeneralItem.propTypes = {
                 text: PropTypes.string,
             }),
         ),
+        formType: PropTypes.string,
     }).isRequired,
 };
 
