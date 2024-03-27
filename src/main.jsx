ad0b13db447749785e924a2cdadd2338491649e7
Wee Jian Hui Kevin <hello@codebykevin.dev>
2024-03-27 21:48:07 +0800

    build: import tooltip library in parent component

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from './App.jsx';
 import './index.css';
 import { LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import 'react-tooltip/dist/react-tooltip.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
