804f883501309a9985cafffa8bad5c5d5d7aeef3
Wee Jian Hui Kevin <hello@codebykevin.dev>
2024-06-09 16:23:30 +0800

    feat: reverse pdf generation implementation
    PDF generation implementation and testing to be
    done on a separate feature branch.
    
    File changes on main branch to be omitted.

--- src/customiser/Customiser.jsx
+++ src/customiser/Customiser.jsx
@@ -5,10 +5,10 @@ import PropTypes from 'prop-types';
 import { jsPDF } from 'jspdf';
 
 // Default export is a4 paper, portrait, using millimeters for units
-const doc = new jsPDF();
+// const doc = new jsPDF();
 
-doc.text('Hello world!', 10, 10);
-doc.save('a4.pdf');
+// doc.text('Hello world!', 10, 10);
+// doc.save('a4.pdf');
 
 const Customiser = ({ cvData, setCvData }) => {
     const setInfo = (newInfo) => {
